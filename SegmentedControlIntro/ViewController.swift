b6ef16ea87ec8ce54e9c290840d79e405fdf5249
Lin LaMonte <penelopelamonte@gmail.com>
2022-02-24 14:05:24 -0700

    Learned  about segmented controls and how to change color of text

--- SegmentedControlIntro/ViewController.swift
+++ SegmentedControlIntro/ViewController.swift
@@ -7,13 +7,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    
+    @IBOutlet weak var helloLabel: UILabel!
+    
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //to change color of text, titleTextAttributes(2) can have different names, segmentControl is the linked segmented control, colors (purple & teal) can change
+        let titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.purple]
+        segmentControl
+            .setTitleTextAttributes(titleTextAttributes, for:.normal)
+        
+        let titleTextAttributes2 = [NSAttributedString.Key.foregroundColor: UIColor.systemTeal]
+        segmentControl
+            .setTitleTextAttributes(titleTextAttributes2, for:.selected)
+        
     }
 
+    @IBAction func segmentController(_ sender: Any) {
+        
+        switch segmentControl.selectedSegmentIndex
+        {
+            
+        case 0:
+            helloLabel.text = "Hello!"
+        case 1:
+            helloLabel.text = "Bonjour!"
+        case 2:
+            helloLabel.text = "Guten Tag!"
+        default:
+            helloLabel.text = "Hello!"
+            
+        }
 
+        
+    }
+    
 }
 
